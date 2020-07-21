0e3f88ee255ad1b1d5e64e0eedc9c6b9a0032eec
jtkoch <jtkoch12@gmail.com>
2020-07-21 15:46:11 -0500

    Nodemailer up and ready to use

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+require('dotenv').config()
+
+const port = 3030;
+
+const index = express();
+index.use(bodyParser.json());
+index.use(bodyParser.urlencoded({ extended: true }));
+index.use(cors());
+
+index.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+index.listen(port, () => {
+    console.log('We are live on port 3030')
+});
+
+index.get('/api', (req, res) => {
+    res.send('API Status: Welcome to Jensen Koch\'s Portfolio API')
+});
+
+index.post('/api/v1', (req,res) => {
+    const data = req.body;
+  
+  const smtpTransport = nodemailer.createTransport({
+    service: 'Gmail',
+    port: 465,
+    auth: {
+      user: process.env.USER_NAME,
+      pass: process.env.PASS
+    }
+  });
+  
+  const mailOptions = {
+    from: data.email,
+    to: process.env.USER_NAME,
+    subject: 'Portfolio Contact Form',
+    html: `<p>${data.name}</p>
+            <p>${data.email}</p>
+            <p>${data.message}</p>`
+  };
+  
+  smtpTransport.sendMail(mailOptions,
+  (error, response) => {
+    if(error) {
+      res.send(error)
+    }else {
+      res.send('Success')
+    }
+    smtpTransport.close();
+  });
+  
+  })
